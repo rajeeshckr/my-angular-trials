038e6aca703dfece2a8ed9daa50ece38a2c07170
Rajeesh Kuttan <rkuttan@flexerasoftware.com>
2016-08-16 17:27:24 +1000

    Again

--- angular2-tour-of-heroes/app/app.component.ts
+++ angular2-tour-of-heroes/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Hero } from './hero';
-import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-app',
@@ -64,25 +64,14 @@ import { HeroDetailComponent } from './hero-detail.component';
     margin-right: .8em;
     border-radius: 4px 0 0 4px;
   }
-`]
+`],
+  providers: [HeroService]
 })
 
-export class AppComponent {  
-  public heroes = HEROES;
-  public selectedHero: Hero;
+export class AppComponent {
   title = 'Tour of Heroes';
+  heroes = Hero[];
+  selectedHero: Hero;
+  constructor(private heroService: HeroService) { }
   onSelect(hero: Hero) { this.selectedHero = hero; }
  }
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
